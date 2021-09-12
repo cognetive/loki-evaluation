35580d34c9b66c19142daf4c3c8ad3d0be725f90
ronenschafferibm <ronen.schaffer@ibm.com>
2021-09-12 10:33:02 +0300

    Sync changes from GHE to GH

--- loki/multi_download_from_loki_with_logcli_using_container.sh
+++ loki/multi_download_from_loki_with_logcli_using_container.sh
@@ -21,20 +21,31 @@ usage: multi_download_file [options]
   exit 0
 }
 
-for i in "$@"
-do
-case $i in
-    -le=*|--loki_endpoint=*) loki_endpoint="${i#*=}"; shift ;;
-    -fh=*|--first_hour=*) first_hour="${i#*=}"; shift ;;
-    -lh=*|--last_hour=*) last_hour="${i#*=}"; shift ;;
-    -d=*|--delay=*) delay="${i#*=}"; shift ;;
-    -h|--help|*) show_usage ;;
-esac
+for i in "$@"; do
+  case $i in
+  -le=* | --loki_endpoint=*)
+    loki_endpoint="${i#*=}"
+    shift
+    ;;
+  -fh=* | --first_hour=*)
+    first_hour="${i#*=}"
+    shift
+    ;;
+  -lh=* | --last_hour=*)
+    last_hour="${i#*=}"
+    shift
+    ;;
+  -d=* | --delay=*)
+    delay="${i#*=}"
+    shift
+    ;;
+  -h | --help | *) show_usage ;;
+  esac
 done
 
 show_configuration() {
 
-echo "
+  echo "
 Note: get more deployment options with -h
 
 Configuration:
@@ -46,37 +57,37 @@ loki_endpoint --> $loki_endpoint
 "
 }
 
-deploy () {
+deploy() {
   oc adm policy add-scc-to-user privileged -z downloadfromloki-service-account
   oc adm policy add-cluster-role-to-user cluster-reader -z downloadfromloki-service-account
   echo "==> Deleting old pods"
   oc delete pod --ignore-not-found=true --wait=true -l appType=downloadfromloki
-  for HOUR in  $(eval echo "{$first_hour..$last_hour}"); do
+  for HOUR in $(eval echo "{$first_hour..$last_hour}"); do
     echo "==> Processing ${DAY}T${HOUR}"
     POD_NAME="downloadfromloki."$DAY"."$HOUR
     FILE_NAME_JSON="/tmp/${FILE_NAME_PRE_JSON}.${DAY}.${HOUR}00.json"
     echo "==> Starting new pod"
     oc process -f $POD_YAML \
-    -p loki_endpoint="$loki_endpoint" \
-    -p file_name_json="$FILE_NAME_JSON" \
-    -p pod_name="$POD_NAME" \
-    | oc apply -f -
-    if [ "$hold_until_completion" = "false" ] ; then
+      -p loki_endpoint="$loki_endpoint" \
+      -p file_name_json="$FILE_NAME_JSON" \
+      -p pod_name="$POD_NAME" |
+      oc apply -f -
+    if [ "$hold_until_completion" = "false" ]; then
       echo "Wait until this download finished"
       while true; do
-        sleep 10  
+        sleep 10
         LINE=$(oc logs $POD_NAME 2>/dev/null | grep "DONE --> ")
-        if [ -n "$LINE" ]; then 
-          echo $LINE;
-          echo $(date)" ==> DOWNLOAD COMPLETE FOR FILE $FILE_NAME_JSON";
-          break;
+        if [ -n "$LINE" ]; then
+          echo $LINE
+          echo $(date)" ==> DOWNLOAD COMPLETE FOR FILE $FILE_NAME_JSON"
+          break
         fi
-        sleep 10  
+        sleep 10
         echo "."
       done
     else
-      echo "waiting $delay seconds before next container is created";
-      sleep $delay;
+      echo "waiting $delay seconds before next container is created"
+      sleep $delay
     fi
   done
 }
@@ -86,32 +97,32 @@ wait_for_complete() {
     echo $(date) "Status report:"
     echo "---------------------"
     completed=0
-    for HOUR in  $(eval echo "{$first_hour..$last_hour}"); do
+    for HOUR in $(eval echo "{$first_hour..$last_hour}"); do
       DOWNLOADER_POD="downloadfromloki."$DAY"."$HOUR
       COMPLETE=$(oc logs $DOWNLOADER_POD 2>/dev/null | grep "DONE --> ")
       echo $COMPLETE
-      if [ -n "$COMPLETE" ] ; then
-        completed=$((completed+1))
+      if [ -n "$COMPLETE" ]; then
+        completed=$((completed + 1))
       fi
     done
-    to_complete=$((last_hour-first_hour+1))
-    if [ $to_complete = $completed ] ; then
-      break;
+    to_complete=$((last_hour - first_hour + 1))
+    if [ $to_complete = $completed ]; then
+      break
     fi
     sleep 30
   done
 }
 
-main () {
+main() {
   show_configuration
   download_start_time=$(date)
-  echo $(date) "===>>>> DOWNLOAD START !!!! ";
+  echo $(date) "===>>>> DOWNLOAD START !!!! "
   deploy
   wait_for_complete
   download_end_time=$(date)
-  start_sec=$(date -d "$download_start_time" +%s);
-  end_sec=$(date -d "$download_end_time" +%s);
-  echo $(date) "===>>>> DOWNLOAD COMPLETE in $(( end_sec-start_sec )) seconds !!!! -- started at $download_start_time ended at $download_end_time";
+  start_sec=$(date -d "$download_start_time" +%s)
+  end_sec=$(date -d "$download_end_time" +%s)
+  echo $(date) "===>>>> DOWNLOAD COMPLETE in $((end_sec - start_sec)) seconds !!!! -- started at $download_start_time ended at $download_end_time"
 }
 
 main
--- loki/multi_upload_file_to_loki_with_promtail_using_container.sh
+++ loki/multi_upload_file_to_loki_with_promtail_using_container.sh
@@ -15,25 +15,30 @@ show_usage() {
   echo "
 usage: multi_upload_file [options]
   options:
-    -le   --loki_endpoint                  Loki distributors endpoint   
+    -le   --loki_endpoint                  Loki distributors endpoint
     -lh   --last_hour=[enum]               Last Logfile hour (default: 00)
     -h,   --help                           Show usage
 "
   exit 0
 }
 
-for i in "$@"
-do
-case $i in
-    -le=*|--loki_endpoint=*) loki_endpoint="${i#*=}"; shift ;;
-    -lh=*|--last_hour=*) last_hour="${i#*=}"; shift ;;
-    -h|--help|*) show_usage ;;
-esac
+for i in "$@"; do
+  case $i in
+  -le=* | --loki_endpoint=*)
+    loki_endpoint="${i#*=}"
+    shift
+    ;;
+  -lh=* | --last_hour=*)
+    last_hour="${i#*=}"
+    shift
+    ;;
+  -h | --help | *) show_usage ;;
+  esac
 done
 
 show_configuration() {
 
-echo "
+  echo "
 Note: get more deployment options with -h
 
 Configuration:
@@ -43,40 +48,40 @@ loki_endpoint --> $loki_endpoint
 "
 }
 
-deploy () {
+deploy() {
   oc adm policy add-scc-to-user privileged -z uploadtoloki-service-account
   oc adm policy add-cluster-role-to-user cluster-reader -z uploadtoloki-service-account
-  for HOUR in  $(eval echo "{00..$last_hour}"); do
+  for HOUR in $(eval echo "{00..$last_hour}"); do
     FILE_NAME_JSON=$FILE_NAME_PRE_JSON"."$DAY"."$HOUR"00.json"
-    DEPLOYMENT_NAME="uploadtoloki."$DAY"."$HOUR
+    POD_NAME="uploadtoloki."$DAY"."$HOUR
     echo "starting container upload for $FILE_NAME_JSON"
-    oc delete deployment --ignore-not-found=true --wait=true $DEPLOYMENT_NAME 
+    oc delete pod --ignore-not-found=true --wait=true $POD_NAME
     oc process -f $DEPLOY_YAML \
-    -p loki_endpoint="$loki_endpoint" \
-    -p file_name_json="$FILE_NAME_JSON" \
-    -p source_s3_bucket="$SOURCE_S3_BUCKET" \
-    -p source_s3_endpoint="$SOURCE_S3_ENDPOINT" \
-    -p source_s3_credentials="$SOURCE_S3_CREDENTIALS" \
-    -p source_s3_folder="$SOURCE_S3_FOLDER" \
-    -p deployment_name="$DEPLOYMENT_NAME" \
-    | oc apply -f -
-    if [ "$hold_until_completion" = "false" ] ; then
+      -p loki_endpoint="$loki_endpoint" \
+      -p file_name_json="$FILE_NAME_JSON" \
+      -p source_s3_bucket="$SOURCE_S3_BUCKET" \
+      -p source_s3_endpoint="$SOURCE_S3_ENDPOINT" \
+      -p source_s3_credentials="$SOURCE_S3_CREDENTIALS" \
+      -p source_s3_folder="$SOURCE_S3_FOLDER" \
+      -p pod_name="$POD_NAME" |
+      oc apply -f -
+    if [ "$hold_until_completion" = "false" ]; then
       echo "Wait until this upload finish"
       while true; do
-        sleep 10  
-        UPLOADER_POD=$(oc get pod -l app=$DEPLOYMENT_NAME -o jsonpath="{.items[0].metadata.name}" 2>/dev/null )
+        sleep 10
+        UPLOADER_POD=$(oc get pod -l app=$POD_NAME -o jsonpath="{.items[0].metadata.name}" 2>/dev/null)
         LINE=$(oc logs $UPLOADER_POD 2>/dev/null | grep "DONE --> ")
-        if [ -n "$LINE" ]; then 
-          echo $LINE;
-          echo $(date)" ==> UPLOAD COMPLETE FOR FILE $FILE_NAME_JSON";
-          break;
+        if [ -n "$LINE" ]; then
+          echo $LINE
+          echo $(date)" ==> UPLOAD COMPLETE FOR FILE $FILE_NAME_JSON"
+          break
         fi
-        sleep 10  
+        sleep 10
         echo "."
       done
     else
-      echo "waiting 180 seconds before next container is created";
-      sleep 180;
+      echo "waiting 180 seconds before next container is created"
+      sleep 180
     fi
   done
 }
@@ -86,31 +91,31 @@ wait_for_complete() {
     echo $(date) "Status report:"
     echo "---------------------"
     completed=0
-    for HOUR in  $(eval echo "{00..$last_hour}"); do
-      DEPLOYMENT_NAME="uploadtoloki."$DAY"."$HOUR
-      UPLOADER_POD=$(oc get pod -l app=$DEPLOYMENT_NAME -o jsonpath="{.items[0].metadata.name}" 2>/dev/null )
+    for HOUR in $(eval echo "{00..$last_hour}"); do
+      POD_NAME="uploadtoloki."$DAY"."$HOUR
+      UPLOADER_POD=$(oc get pod -l app=$POD_NAME -o jsonpath="{.items[0].metadata.name}" 2>/dev/null)
       COMPLETE=$(oc logs $UPLOADER_POD 2>/dev/null | grep "DONE --> ")
       echo $COMPLETE
-      if [ -n "$COMPLETE" ] ; then
-        completed=$((completed+1))
+      if [ -n "$COMPLETE" ]; then
+        completed=$((completed + 1))
       fi
     done
-    to_complete=$((last_hour+1))
-    if [ $to_complete = $completed ] ; then
-      break;
+    to_complete=$((last_hour + 1))
+    if [ $to_complete = $completed ]; then
+      break
     fi
-    sleep 30  
+    sleep 30
   done
 }
 
-main () {
+main() {
   show_configuration
   upload_start_time=$(date)
-  echo $(date) "===>>>> UPLOAD START !!!! ";
+  echo $(date) "===>>>> UPLOAD START !!!! "
   deploy
   wait_for_complete
   upload_end_time=$(date)
-  echo $(date) "===>>>> UPLOAD COMPLETE !!!! -- started at $upload_start_time ended at $upload_end_time";
+  echo $(date) "===>>>> UPLOAD COMPLETE !!!! -- started at $upload_start_time ended at $upload_end_time"
 }
 
 main
